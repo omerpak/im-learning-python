78c2d1b0acd46c7c857634cfb55f8f8e678e8119
Omer Faruk PAK <37738031+omerpak@users.noreply.github.com>
2018-06-04 14:10:59 +0300

    Update I spelt.py

--- I spelt.py
+++ I spelt.py
@@ -1,7 +1,11 @@
+#it asks to user his/her name
 name = input("What is your name : ")
+#our variable > we will know letter number of his/her name with this variable
 x=0
+#while x is less than letter number of name print the letter and add 1 to x
 while x < len(name):
     print(name[x])
     x += 1
+#when program spelt the name it will write to screen I spelt
 else:
     print("I spelt")
