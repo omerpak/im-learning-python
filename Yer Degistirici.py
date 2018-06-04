a263c70462bf8487f15af93960f1ea9f10e7cb6d
Omer Faruk PAK <37738031+omerpak@users.noreply.github.com>
2018-06-04 14:02:37 +0300

    Add files via upload

---
+++ Yer Degistirici.py
@@ -0,0 +1,14 @@
+#iki degisken olusturduk
+num1 = 30
+num2 = 10
+#burda ilk hallerini sollarında kacinci sayı olduklarını format metoduyla ekrana yazdırdık
+print('İlk Sayı:{0} '.format(num1))
+print('İkinci Sayı:{0} '.format(num2))
+print('\n')
+#yerlerini degistirdik biraz matematik
+num1=num1+num2;  
+num2=num1-num2;  
+num1=num1-num2;
+#yerleri degismis hallerini yine format metoduyla ekrana bastırdık
+print('İlk Sayı:{0} '.format(num1))
+print('İkinci Sayı:{0} '.format(num2))
